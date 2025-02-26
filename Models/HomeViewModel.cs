df5ee679ba09b68c176b01344b89facea7cca397
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2025-02-26 20:58:17 +0600

    Create HomeViewModel.cs

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace LostandFound.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<LostandFound.Models.LostItem> LostItems { get; set; }
+        public List<LostandFound.Models.FoundItem> FoundItems { get; set; }
+    }
+}
